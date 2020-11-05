420ade8374969f83127e1fa233073e048e8d8698
Pooya <pooya.rezaee.official@gmail.com>
2020-11-05 19:08:29 +0330

    Change Colors ui for more beautiful

--- Ui_App.py
+++ Ui_App.py
@@ -5,14 +5,15 @@ import Backend_App as Commands
 root = Tk()
 root.title("Get Screen Shot")
 root.resizable(width=False,height=False)
+root.configure(bg="white")
 # ============= Widget ==================
-text_enter_format = Label(root,text="Enter Your Format")
+text_enter_format = Label(root,text="Enter Your Format : ",bg="white")
 text_enter_format.grid(row=0,column=0)
 
 input_format = Entry(root)
 input_format.grid(row=0,column=1)
 
-btn_screen_shot = Button(root,text="Get Screen Shot",command=lambda : Commands.get_screen_shot(format_picture=input_format.get()))
+btn_screen_shot = Button(root,text="Get Screen Shot",bg="#e8ffea",command=lambda : Commands.get_screen_shot(format_picture=input_format.get()))
 btn_screen_shot.grid(row=1,column=0,columnspan=2)
 # ============= Loop ==============
 root.mainloop()
