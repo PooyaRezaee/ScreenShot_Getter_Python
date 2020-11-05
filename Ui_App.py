f88da4107239b15ea3f5bcfe21747c22e976650a
Pooya <pooya.rezaee.official@gmail.com>
2020-11-05 11:47:28 +0330

    Combine Backend on Ui

--- Ui_App.py
+++ Ui_App.py
@@ -1,11 +1,12 @@
 # =========== Librarys =========
 from tkinter import *
+import Backend_App as Commands
 # =========== Setting ==========
 root = Tk()
 root.title("Get Screen Shot")
 root.resizable(width=False,height=False)
 # ============= Widget ==================
-btn_screen_shot = Button(root,text="Get Screen Shot")
+btn_screen_shot = Button(root,text="Get Screen Shot",command=Commands.get_screen_shot)
 btn_screen_shot.grid(row=0,column=0)
 # ============= Loop ==============
 root.mainloop()
