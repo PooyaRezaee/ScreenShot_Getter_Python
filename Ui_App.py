c963d78bccedf81edfcce9cfe204cd4cef23be0f
Pooya <pooya.rezaee.official@gmail.com>
2020-11-05 11:16:05 +0330

    Created Ui App

---
+++ Ui_App.py
@@ -0,0 +1,12 @@
+# =========== Librarys =========
+from tkinter import *
+# =========== Setting ==========
+root = Tk()
+root.title("Get Screen Shot")
+root.resizable(width=False,height=False)
+# ============= Widget ==================
+btn_screen_shot = Button(root,text="Get Screen Shot")
+btn_screen_shot.grid(row=0,column=0)
+# ============= Loop ==============
+root.mainloop()
+# =E=N=D======= Loop ==============
